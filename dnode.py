41af73230616057b2a8d0dd3237e309b43411266
farahkazmi <fkazmi.email@gmail.com>
2025-01-21 16:22:48 +0530

    Added dnode.py

---
+++ dnode.py
@@ -0,0 +1,12 @@
+""""
+DNode class : depicts a node in a double linked list
+Methods : print(self)
+"""
+class DNode:
+    def __init__(self, value):
+        self.value = value
+        self.previousNode = None
+        self.nextNode = None
+        
+    def print(self):
+        print(self.value)
